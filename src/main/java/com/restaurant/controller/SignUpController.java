70d8ff53dda97c3ab00630f737e08f494cfb1123
Madhavi Unnam <madhuunnam@gmail.com>
2016-02-21 11:03:10 -0500

    Remove redundant Imports

--- src/main/java/com/restaurant/controller/SignUpController.java
+++ src/main/java/com/restaurant/controller/SignUpController.java
@@ -1,8 +1,5 @@
 package com.restaurant.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;

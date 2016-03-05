0ade0d69e49d524163708b391ca658b7a8a375b6
madhaviunnam <madhaviunnam@192.168.1.5>
2016-03-05 08:55:11 -0500

    removed unnecessary onclick functions

--- src/main/resources/static/javascript/searchPage.js
+++ src/main/resources/static/javascript/searchPage.js
@@ -5,27 +5,14 @@ function onSearch() {
 function onAddToMyAddress(){
 	alert("Clicked Add to My Address");
 }
-function onSignUpTypeCust() {
-	document.getElementById("signUpAsCust").action = "/getCustomerSignUpPage";
-	document.getElementById("signUpAsCust").submit();
-}
-function onSignUpTypeCust() {
-	document.getElementById("signUpAsRest").action = "/getRestaurantSignUpPage";
-	document.getElementById("signUpAsRest").submit();
-}
-function onSignUpTypeCust() {
-	document.getElementById("signUpAsAssc").action = "/getAssociateSignUpPage";
-	document.getElementById("signUpAsAssc").submit();
-}
-function onSignUpTypeAdmin() {
-	document.getElementById("signUpAsAdmin").action = "/getAdminSignUpPage";
-	document.getElementById("signUpAsAdmin").submit();
-}
+
 $(document).ready(function() {
+	
 $('#restListTable > tbody > tr').click(function() {
     document.getElementById("restTableForm").action = "/getRestaurantDetailsPage";
 	document.getElementById("restTableForm").submit();
 });
+
 });
 
 

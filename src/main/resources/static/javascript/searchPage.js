616d0249eb2125c7c3d90277e293dc7c4cd02d1c
madhaviunnam <madhaviunnam@192.168.1.5>
2016-03-16 09:49:32 -0400

    Js changes for getting fav Rests for customer

--- src/main/resources/static/javascript/searchPage.js
+++ src/main/resources/static/javascript/searchPage.js
@@ -6,13 +6,29 @@ function onAddToMyAddress(){
 	alert("Clicked Add to My Address");
 }
 
+function myFavorites(sessionUserId){
+	 $.ajax({
+	        url: "http://localhost:8090/getFavRestaurantsForCustomer/"+sessionUserId,
+	        type: "GET",
+	        cache: false,
+	        dataType: "json",
+	        crossDomain: true
+	    }).then(function(data) {
+	    	if($('#favListTable tr').length == 1){
+	    		$.each(data, function(index, element) {
+	    			$('#favListTable').append('<tr><td>'+element.restId+'</td>'+'<td>'+element.restName+'</td></tr>');
+	    		});
+	    	}
+	    });
+}
+
 $(document).ready(function() {
 	
-$('#restListTable > tbody > tr').click(function() {
-    document.getElementById("restTableForm").action = "/getRestaurantDetailsPage";
-	document.getElementById("restTableForm").submit();
-});
-
+	$('#restListTable > tbody > tr').click(function() {
+	    document.getElementById("restTableForm").action = "/getRestaurantDetailsPage";
+		document.getElementById("restTableForm").submit();
+	});
+	
 });
 
 
